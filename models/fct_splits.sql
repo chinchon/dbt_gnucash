3da82490ebe9913c61ad74299bc22db2e2173e20
ChinChon Na <chinchon91@gmail.com>
2023-10-10 01:03:50 +0800

    Create fct_splits.sql

---
+++ models/fct_splits.sql
@@ -0,0 +1,5 @@
+SELECT 
+    s.guid, s.account_guid, a.name, t.date, value_num / value_denom AS value
+FROM {{ source("public", "splits") }} s
+LEFT JOIN {{ ref("fct_transactions") }} t ON t.guid = tx_guid
+LEFT JOIN {{ source("public", "accounts") }} a ON a.guid = s.account_guid
