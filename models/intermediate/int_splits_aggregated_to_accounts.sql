38c23144fd586d9ac739f00baf0db16612faaed4
ChinChon Na <chinchon91@gmail.com>
2024-04-20 21:29:11 +0800

    Update int_splits_aggregated_to_accounts.sql

--- models/intermediate/int_splits_aggregated_to_accounts.sql
+++ models/intermediate/int_splits_aggregated_to_accounts.sql
@@ -2,6 +2,7 @@ select
     account_guid as guid,
     max(
         case reconcile_state when 'y' then reconcile_date else null end
-    ) as last_reconcile_date
+    ) as last_reconcile_date,
+    count(*) as count_of_splits
 from {{ ref("stg_splits") }}
 group by 1
