a545cb9c2dde6bef3180705481d7528ee8e5b21a
ChinChon Na <chinchon91@gmail.com>
2024-04-20 20:53:10 +0800

    Update int_accounts_joined_parent_accounts.sql

--- models/intermediate/int_accounts_joined_parent_accounts.sql
+++ models/intermediate/int_accounts_joined_parent_accounts.sql
@@ -1,9 +1,15 @@
 {% set max_parent = 10 %}
 select
     l1.guid,
-    l1.name,
+    concat_ws(
+        ':',
+        {% for i in range(1, max_parent)|reverse %}
+            nullif(l{{ i }}.name, 'Root Account'){% if not loop.last %},{% endif %}
+        {% endfor %}
+    ) as fqn,
+    l1.name as l1_name,
     {% for i in range(2, max_parent) %}
-        l{{ i }}.name{% if not loop.last %},{% endif %}
+        l{{ i }}.name as l{{ i }}_name{% if not loop.last %},{% endif %}
     {% endfor %}
 from int_accounts_trimmed l1
 {% for i in range(2, max_parent) %}
