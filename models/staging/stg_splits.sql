f4f9fdbc6fb7a2d1ae1faf4798f60075c15bfcef
ChinChon Na <chinchon91@gmail.com>
2024-04-22 00:22:29 +0800

    Update stg_splits.sql

--- models/staging/stg_splits.sql
+++ models/staging/stg_splits.sql
@@ -2,6 +2,7 @@ with
     splits as (
         select
             *,
+            cast(reconcile_state = 'y' as int) as is_reconciled,
             cast(cast(value_num as double) / value_denom as double) as value,
             cast(cast(quantity_num as double) / quantity_denom as double) as quantity
         from {{ source("gnucash", "splits") }}
