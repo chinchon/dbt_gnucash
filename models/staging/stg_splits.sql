c4284090c9f50edc994fbc60a050800cac57da73
ChinChon Na <chinchon91@gmail.com>
2024-04-22 00:12:47 +0800

    Update stg_splits.sql

--- models/staging/stg_splits.sql
+++ models/staging/stg_splits.sql
@@ -1,1 +1,10 @@
-select * from {{ source("gnucash", "splits") }}
+with
+    splits as (
+        select
+            *,
+            cast(cast(value_num as double) / value_denom as double) as value,
+            cast(cast(quantity_num as double) / quantity_denom as double) as quantity
+        from {{ source("gnucash", "splits") }}
+    )
+select *, cast(value = quantity as int) as is_value_equal_quantity
+from splits
