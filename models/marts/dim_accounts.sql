60649618958cba95962906eaef88af2b093227db
chinchon <chinchon91@gmail.com>
2024-04-22 00:25:05 +0800

    Dev (#12)
    * Update .gitignore
    
    * Update dbt_project.yml
    
    * Update int_splits_aggregated_to_accounts.sql
    
    * Update dim_accounts.sql
    
    * Update rpt_accounts_to_reconcile.sql
    
    * Update rpt_accounts_to_reconcile.sql
    
    * Update int_splits_aggregated_to_accounts.sql
    
    * Update dbt_project.yml
    
    * Update sources.yml
    
    * Create profiles.yml.example
    
    * Update int_splits_aggregated_to_accounts.sql
    
    * Update dim_accounts.sql
    
    * Create rpt_account_balances.sql
    
    * Create stg_commodities.sql
    
    * Update stg_splits.sql
    
    * Update stg_splits.sql
    
    * Update int_splits_aggregated_to_accounts.sql

--- models/marts/dim_accounts.sql
+++ models/marts/dim_accounts.sql
@@ -1,5 +1,5 @@
 select
-    guid,
+    a.guid,
     name,
     fqn,
     account_type,
@@ -7,7 +7,10 @@ select
     last_reconcile_date,
     count_of_splits,
     count_of_splits_reconciled,
-    count_of_splits_unreconciled
-from {{ ref("stg_accounts") }}
+    count_of_splits_unreconciled,
+    balance,
+    c.mnemonic as unit
+from {{ ref("stg_accounts") }} a
 left join {{ ref("int_splits_aggregated_to_accounts") }} using (guid)
 left join {{ ref("int_accounts_joined_parent_accounts") }} using (guid)
+left join {{ ref("stg_commodities") }} c on a.commodity_guid = c.guid
--- models/staging/stg_splits.sql
+++ models/staging/stg_splits.sql
@@ -1,1 +1,11 @@
-select * from {{ source("gnucash", "splits") }}
+with
+    splits as (
+        select
+            *,
+            cast(reconcile_state = 'y' as int) as is_reconciled,
+            cast(cast(value_num as double) / value_denom as double) as value,
+            cast(cast(quantity_num as double) / quantity_denom as double) as quantity
+        from {{ source("gnucash", "splits") }}
+    )
+select *, cast(value = quantity as int) as is_value_equal_quantity
+from splits
