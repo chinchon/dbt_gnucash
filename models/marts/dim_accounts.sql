fd2f73d062b7445ef878e4a3e4ff742dcb5f45c4
ChinChon Na <chinchon91@gmail.com>
2024-04-21 17:57:29 +0800

    Update dim_accounts.sql

--- models/marts/dim_accounts.sql
+++ models/marts/dim_accounts.sql
@@ -1,5 +1,13 @@
 select
-    guid, name, fqn, account_type, is_placeholder, last_reconcile_date, count_of_splits
+    guid,
+    name,
+    fqn,
+    account_type,
+    is_placeholder,
+    last_reconcile_date,
+    count_of_splits,
+    count_of_splits_reconciled,
+    count_of_splits_unreconciled
 from {{ ref("stg_accounts") }}
 left join {{ ref("int_splits_aggregated_to_accounts") }} using (guid)
 left join {{ ref("int_accounts_joined_parent_accounts") }} using (guid)
