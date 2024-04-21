9b124e4ed85f691f31bdfaf642c588671919aa28
chinchon <chinchon91@gmail.com>
2024-04-21 23:35:42 +0800

    Dev (#11)
    * Update .gitignore
    
    * Update dbt_project.yml
    
    * Update int_splits_aggregated_to_accounts.sql
    
    * Update dim_accounts.sql
    
    * Update rpt_accounts_to_reconcile.sql
    
    * Update rpt_accounts_to_reconcile.sql
    
    * Update int_splits_aggregated_to_accounts.sql
    
    * Update dbt_project.yml
    
    * Update sources.yml
    
    * Create profiles.yml.example

--- models/intermediate/int_splits_aggregated_to_accounts.sql
+++ models/intermediate/int_splits_aggregated_to_accounts.sql
@@ -4,7 +4,7 @@ select
         case reconcile_state when 'y' then reconcile_date else null end
     ) as last_reconcile_date,
     count(*) as count_of_splits,
-    sum(reconcile_state = 'y') as count_of_splits_reconciled,
-    sum(reconcile_state != 'y') as count_of_splits_unreconciled
+    sum(cast(reconcile_state = 'y' as int)) as count_of_splits_reconciled,
+    sum(cast(reconcile_state != 'y' as int)) as count_of_splits_unreconciled
 from {{ ref("stg_splits") }}
 group by 1
