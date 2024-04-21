dbcdc5258936b6f0600417d3d208900ff4103707
ChinChon Na <chinchon91@gmail.com>
2024-04-21 23:13:49 +0800

    Update rpt_accounts_to_reconcile.sql

--- models/marts/rpt_accounts_to_reconcile.sql
+++ models/marts/rpt_accounts_to_reconcile.sql
@@ -2,6 +2,6 @@ select *
 from {{ ref("dim_accounts") }}
 where
     not is_placeholder
-    and count_of_splits_unreconciled > 0
+    and count_of_splits > 0
     and account_type not in ('INCOME', 'EXPENSE', 'TRADING', 'EQUITY')
 order by count_of_splits_unreconciled desc, last_reconcile_date
