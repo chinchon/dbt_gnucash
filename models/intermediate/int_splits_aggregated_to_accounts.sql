714a6e6c57254fad1c7174963b97f61f0a3c9d10
ChinChon Na <chinchon91@gmail.com>
2024-05-20 22:37:43 +0800

    int models

---
+++ models/intermediate/int_splits_aggregated_to_accounts.sql
@@ -0,0 +1,13 @@
+select
+    account_guid as guid,
+    max(
+        case when is_reconciled = 1 then reconcile_date else null end
+    ) as last_reconcile_date,
+    count(*) as count_of_splits,
+    sum(is_reconciled) as count_of_splits_reconciled,
+    sum(case when is_reconciled = 1 then 0 else 1 end) as count_of_splits_unreconciled,
+    cast(sum(quantity) as double) as balance,
+    t.post_date as latest_transaction
+from {{ ref("stg_splits") }} s
+left join {{ ref("stg_transactions") }} t on t.guid = s.tx_guid
+group by 1
