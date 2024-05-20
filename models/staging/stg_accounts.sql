2937ff08a50f9f74150098c78cbd8cd510e94255
ChinChon Na <chinchon91@gmail.com>
2024-05-20 21:58:09 +0800

    Update stg_accounts.sql

--- models/staging/stg_accounts.sql
+++ models/staging/stg_accounts.sql
@@ -1,1 +1,24 @@
-select *, placeholder = 1 as is_placeholder from {{ source("gnucash", "accounts") }}
+with
+    source as (select * from {{ source('gnucash', 'accounts') }}),
+
+    renamed as (
+
+        select
+            guid,
+            name,
+            account_type,
+            commodity_guid,
+            commodity_scu,
+            non_std_scu,
+            parent_guid,
+            code,
+            description,
+            hidden,
+            placeholder
+
+        from source
+
+    )
+
+select *, placeholder = 1 as is_placeholder
+from renamed
