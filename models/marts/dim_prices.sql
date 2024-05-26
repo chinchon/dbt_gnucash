ed7eb2df5b4935ca7c61a7e724cdcc7176a0adeb
ChinChon Na <chinchon91@gmail.com>
2024-05-26 22:22:24 +0800

    Update dim_prices.sql

--- models/marts/dim_prices.sql
+++ models/marts/dim_prices.sql
@@ -12,22 +12,27 @@
 
 {%- set usd_myr = dbt_utils.get_single_value(sql_statement) -%}
 
-select
-    guid,
-    commodity_namespace,
-    commodity_mnemonic,
-    currency_mnemonic,
-    value,
-    date,
-    case
-        when commodity_mnemonic = 'MYR'
-        then 1
-        when currency_mnemonic = 'MYR'
-        then value
-        when currency_mnemonic = 'SGD'
-        then value * {{ sgd_myr }}
-        when currency_mnemonic = 'USD'
-        then value * {{ usd_myr }}
-    end as value_myr
-from {{ ref("int_prices_joined_commodities") }}
-where is_latest
+with
+    int_prices_joined_commodities as (
+        select
+            guid,
+            commodity_namespace,
+            commodity_mnemonic,
+            currency_mnemonic,
+            value,
+            date,
+            case
+                when commodity_mnemonic = 'MYR'
+                then 1
+                when currency_mnemonic = 'MYR'
+                then value
+                when currency_mnemonic = 'SGD'
+                then value * {{ sgd_myr }}
+                when currency_mnemonic = 'USD'
+                then value * {{ usd_myr }}
+            end as value_myr
+        from {{ ref("int_prices_joined_commodities") }}
+        where is_latest
+    )
+select *, value_myr / {{ sgd_myr }} as value_sgd, value_myr / {{ usd_myr }} as value_usd
+from int_prices_joined_commodities
