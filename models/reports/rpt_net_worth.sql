49033bc93630b1501196d7063553d20dda732c72
ChinChon Na <chinchon91@gmail.com>
2024-05-26 22:22:37 +0800

    Update rpt_net_worth.sql

--- models/reports/rpt_net_worth.sql
+++ models/reports/rpt_net_worth.sql
@@ -1,3 +1,6 @@
-select sum(balance_myr) as balance_myr
+select
+    sum(balance_myr) as balance_myr,
+    sum(balance_sgd) as balance_sgd,
+    sum(balance_usd) as balance_usd
 from {{ ref("rpt_balance_sheet") }}
 where account_category in ('Assets', 'Liabilities')
