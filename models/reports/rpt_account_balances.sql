f1b98422116d6f17091aec3e00223bd890d45474
ChinChon Na <chinchon91@gmail.com>
2024-05-26 22:22:27 +0800

    Update rpt_account_balances.sql

--- models/reports/rpt_account_balances.sql
+++ models/reports/rpt_account_balances.sql
@@ -6,6 +6,8 @@ select
     a.latest_transaction,
     a.unit,
     round(a.balance, 2) as balance,
-    round(a.balance * value_myr, 2) as balance_myr
+    round(a.balance * value_myr, 2) as balance_myr,
+    round(a.balance * value_sgd, 2) as balance_sgd,
+    round(a.balance * value_usd, 2) as balance_usd
 from {{ ref("dim_accounts") }} a
 left join {{ ref("dim_prices") }} p on p.commodity_mnemonic = a.unit
