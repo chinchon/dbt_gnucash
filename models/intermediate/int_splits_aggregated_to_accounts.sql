99456c76fc0fb61c4a1f72ef94089153cf75fa50
ChinChon Na <chinchon91@gmail.com>
2024-05-27 02:19:45 +0800

    Update int_splits_aggregated_to_accounts.sql

--- models/intermediate/int_splits_aggregated_to_accounts.sql
+++ models/intermediate/int_splits_aggregated_to_accounts.sql
@@ -6,7 +6,16 @@ select
     count(*) as count_of_splits,
     sum(is_reconciled) as count_of_splits_reconciled,
     sum(case when is_reconciled = 1 then 0 else 1 end) as count_of_splits_unreconciled,
-    sum(quantity) as balance,
+    sum(
+        case
+            when
+                strftime('%Y-%m-%d', post_date)
+                <= strftime('%Y-%m-%d', datetime(datetime('now'), 'localtime'))
+            then quantity
+            else 0
+        end
+    ) as balance,
+    sum(quantity) as balance_incl_future,
     t.post_date as latest_transaction
 from {{ ref("stg_splits") }} s
 left join {{ ref("stg_transactions") }} t on t.guid = s.tx_guid
