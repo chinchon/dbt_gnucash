334741810097e00cb60b12b2f9aa55c60cb21dd3
ChinChon Na <chinchon91@gmail.com>
2024-05-27 00:24:24 +0800

    Update dim_accounts.sql

--- models/marts/dim_accounts.sql
+++ models/marts/dim_accounts.sql
@@ -13,11 +13,15 @@ select
     count_of_splits,
     count_of_splits_reconciled,
     count_of_splits_unreconciled,
-    balance,
-    c.mnemonic as unit,
     latest_transaction,
+    c.mnemonic as unit,
+    balance,
+    balance * value_myr as balance_myr,
+    balance * value_sgd as balance_sgd,
+    balance * value_usd as balance_usd,
     parent_guid
 from {{ ref("stg_accounts") }} a
 left join {{ ref("int_splits_aggregated_to_accounts") }} using (guid)
 left join {{ ref("int_accounts_joined_parent_accounts") }} using (guid)
 left join {{ ref("stg_commodities") }} c on a.commodity_guid = c.guid
+left join {{ ref("dim_prices") }} p on p.commodity_mnemonic = c.mnemonic
