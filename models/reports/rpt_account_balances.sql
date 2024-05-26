1608414cc0826f54b90076ee4d42c51211a81c1f
ChinChon Na <chinchon91@gmail.com>
2024-05-27 00:24:19 +0800

    Update rpt_account_balances.sql

--- models/reports/rpt_account_balances.sql
+++ models/reports/rpt_account_balances.sql
@@ -6,9 +6,8 @@ select
     a.latest_transaction,
     a.unit,
     round(a.balance, 2) as balance,
-    round(a.balance * value_myr, 2) as balance_myr,
-    round(a.balance * value_sgd, 2) as balance_sgd,
-    round(a.balance * value_usd, 2) as balance_usd,
+    round(a.balance_myr, 2) as balance_myr,
+    round(a.balance_sgd, 2) as balance_sgd,
+    round(a.balance_usd, 2) as balance_usd,
     parent_guid
 from {{ ref("dim_accounts") }} a
-left join {{ ref("dim_prices") }} p on p.commodity_mnemonic = a.unit
