bf12274cd46ccf1e294a25500a49a26f435e770f
ChinChon Na <chinchon91@gmail.com>
2024-05-26 22:09:52 +0800

    move to reports folder

--- models/reports/rpt_account_balances.sql
+++ models/reports/rpt_account_balances.sql
@@ -2,9 +2,10 @@ select
     a.guid,
     a.name,
     a.fqn,
-    round(a.balance, 2) as balance,
-    a.unit,
+    a.account_category,
     a.latest_transaction,
+    a.unit,
+    round(a.balance, 2) as balance,
     round(a.balance * value_myr, 2) as balance_myr
 from {{ ref("dim_accounts") }} a
 left join {{ ref("dim_prices") }} p on p.commodity_mnemonic = a.unit
