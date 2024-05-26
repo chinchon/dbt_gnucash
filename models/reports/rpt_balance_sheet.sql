3c6640455d8b2cb0be30355164ec7d64795810e3
ChinChon Na <chinchon91@gmail.com>
2024-05-26 22:22:33 +0800

    Update rpt_balance_sheet.sql

--- models/reports/rpt_balance_sheet.sql
+++ models/reports/rpt_balance_sheet.sql
@@ -1,3 +1,7 @@
-select account_category, sum(balance_myr) as balance_myr
+select
+    account_category,
+    sum(balance_myr) as balance_myr,
+    sum(balance_sgd) as balance_sgd,
+    sum(balance_usd) as balance_usd
 from {{ ref("rpt_account_balances") }}
 group by 1
